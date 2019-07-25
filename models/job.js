854fe08acea13540826a08fbfa09746dbb45fdb5
Mubin Salehin <mubin.salehin@gmail.com>
2019-07-25 22:45:16 +0000

    Make job.status default to 'archive'

--- models/job.js
+++ models/job.js
@@ -8,7 +8,7 @@ var jobSchema = mongoose.Schema({
 	description: {type: String, required: true},
 	createdAt: {type: Date, default: Date.now},
 	deadline: {type: Date, required: true},
-	status: {type: String},
+	status: {type: String, default: "archive"},
 	owner: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "User",
